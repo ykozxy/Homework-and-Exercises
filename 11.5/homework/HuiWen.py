2bcf34a67008816c66fa6056cccad97a4067584e
Nick Zhou <ykozxy@users.noreply.github.com>
2017-11-07 22:46:24 +0800

    11.7

---
+++ 11.5/homework/HuiWen.py
@@ -0,0 +1,8 @@
+length = 1
+
+
+def huiwen(string):
+    global length
+
+    def calculate(str1, str2):
+        pass
---
+++ 11.5/homework/max_subarray_new.py
@@ -0,0 +1,21 @@
+# [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+def add_lst(lst):
+    sum1 = 0
+    for num in lst:
+        sum1 += num
+    return sum1
+
+
+def max_sub_array(lst):
+    maxnum = lst[0]
+    for long in range(0, len(lst)):
+        for position in range(0, len(lst)-long):
+            sumnum = add_lst(lst[position: position + long])
+            if sumnum > maxnum:
+                maxnum = sumnum
+    return maxnum
+
+
+print(max_sub_array([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+
+# 优化到O(n)
