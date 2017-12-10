ee1bf206fcc00fae05fc7ce2a2c5ef8c959c8220
Nick Zhou <ykozxy@users.noreply.github.com>
2017-12-10 13:48:53 +0800

    12.10

--- 12.3/homework/is_BST.py
+++ 12.3/homework/is_BST.py
@@ -17,11 +17,35 @@ class TreeNode:
 
 
 def is_bst(node):
-    '''
+    """
     :type node:TreeNode
     :param node:
     :return:
-    '''
+    """
+    if not node:
+        return True
+    current_key = node.key
     if node.is_leaf():
-        pass
+        return True
+    elif node.has_left_child() and node.has_right_child():
+        if node.leftChild.key >= current_key:
+            return False
+        if node.rightChild.key <= current_key:
+            return False
+        return is_bst(node.leftChild) and is_bst(node.rightChild)
+    elif node.has_left_child() and not node.has_right_child():
+        if node.leftChild.key >= current_key:
+            return False
+        return is_bst(node.leftChild) and is_bst(node.rightChild)
+    elif not node.has_left_child() and node.has_right_child():
+        if node.rightChild.key <= current_key:
+            return False
+        return is_bst(node.leftChild) and is_bst(node.rightChild)
 
+
+a = TreeNode(3, 3)
+a.leftChild = TreeNode(1, 1)
+a.rightChild = TreeNode(5, 5)
+a.leftChild.leftChild = TreeNode(0, 1)
+
+print(is_bst(a))
